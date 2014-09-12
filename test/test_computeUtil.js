ad7eafc1d210d7aebd20db1e10f58a3b559bdaa5
Lee <jessy1092@gmail.com>
2014-09-13 01:00:13 +0800

    multiply() function unit test.

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -1,6 +1,21 @@
-var computeUtil = require('../lib/ComputeUtil');
+var ComputeUtil = require('../lib/ComputeUtil');
 var should   = require('should');
+
+var computeUtil = new ComputeUtil();
 
 describe('Util Function', function () {
 
+    describe('#multiply()', function () {
+        it('should multiply two numbers', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.multiply(num1, num2).should.be.equal(num1 * num2);
+        });
+
+        it('show be a number', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.multiply(num1, num2).should.be.a.Number;
+        });
+    });
 });
