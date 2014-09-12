d73f2ae436f29bf0cdf8bb11675dcdbbea99797e
Lee <jessy1092@gmail.com>
2014-09-13 01:11:30 +0800

    minus() function unit test.

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -1,6 +1,21 @@
-var computeUtil = require('../lib/ComputeUtil');
+var ComputeUtil = require('../lib/ComputeUtil');
 var should   = require('should');
+
+var computeUtil = new ComputeUtil();
 
 describe('Util Function', function () {
 
+    describe('#minus()', function () {
+        it('should minus two numbers', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.minus(num1, num2).should.be.equal(num1 - num2);
+        });
+
+        it('show be a number', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.minus(num1, num2).should.be.a.Number;
+        });
+    });
 });
