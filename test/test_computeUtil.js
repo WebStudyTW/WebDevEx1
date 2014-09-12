afd01aa303d6084ae9c6551111f6c8d310457f66
Lee <jessy1092@gmail.com>
2014-09-13 01:24:30 +0800

    stringSplit() function unit function.

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -1,6 +1,21 @@
-var computeUtil = require('../lib/ComputeUtil');
+var ComputeUtil = require('../lib/ComputeUtil');
 var should   = require('should');
+
+var computeUtil = new ComputeUtil();
 
 describe('Util Function', function () {
 
+    describe('#stringSplit()', function () {
+        it('should stringSplit two numbers', function () {
+            var string = 'R123';
+            computeUtil.stringSplit(string).should.be.eql({
+                '0': 'R', '1': '1', '2': '2', '3': '3'
+            });
+        });
+
+        it('show be a number', function () {
+            var string = 'R123';
+            computeUtil.stringSplit(string).should.be.a.Array;
+        });
+    });
 });
