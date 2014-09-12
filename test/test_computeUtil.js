5962c43c121a37c533cbc3427ebe128f54f505fe
Lee <jessy1092@gmail.com>
2014-09-13 01:02:26 +0800

    divide() function unit test.

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -1,6 +1,21 @@
-var computeUtil = require('../lib/ComputeUtil');
+var ComputeUtil = require('../lib/ComputeUtil');
 var should   = require('should');
+
+var computeUtil = new ComputeUtil();
 
 describe('Util Function', function () {
 
+    describe('#divide()', function () {
+        it('should divide two numbers', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.divide(num1, num2).should.be.equal(num1 / num2);
+        });
+
+        it('show be a number', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.divide(num1, num2).should.be.a.Number;
+        });
+    });
 });
