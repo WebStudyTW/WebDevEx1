35f7cf2f2f2ae93d1454142b2e8be2f1f24a9c99
Lee <jessy1092@gmail.com>
2014-09-13 00:56:54 +0800

    add() Function Unit Test
    Signed-off-by: Lee <jessy1092@gmail.com>

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -1,6 +1,21 @@
-var computeUtil = require('../lib/ComputeUtil');
+var ComputeUtil = require('../lib/ComputeUtil');
 var should   = require('should');
+
+var computeUtil = new ComputeUtil();
 
 describe('Util Function', function () {
 
+    describe('#add()', function () {
+        it('should add two numbers', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.add(num1, num2).should.be.equal(num1 + num2);
+        });
+
+        it('show be a number', function () {
+            var num1 = Math.floor(Math.random() * 100);
+            var num2 = Math.floor(Math.random() * 100);
+            computeUtil.add(num1, num2).should.be.a.Number;
+        });
+    });
 });
