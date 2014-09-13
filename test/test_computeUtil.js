edd6a871081319f940e69e58850922eff70c24aa
Lee <jessy1092@gmail.com>
2014-09-13 16:36:22 +0800

    fix test.

--- test/test_computeUtil.js
+++ test/test_computeUtil.js
@@ -11,15 +11,13 @@ describe('Util Function', function () {
 
 
 
-
-
-
     describe('#multiply()', function () {
         it('should multiply two numbers', function () {
             var num1 = Math.floor(Math.random() * 100);
             var num2 = Math.floor(Math.random() * 100);
             computeUtil.multiply(num1, num2).should.be.equal(num1 * num2);
         });
+    });
 
     describe('#add()', function () {
         it('should add two numbers', function () {
